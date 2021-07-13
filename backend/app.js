bddb429f7841c6d09038f988d0952cae5d8b0fb0
trymycode <imitasingha1994@gmail.com>
2021-07-13 21:31:08 +0530

    Completed CRUD operation for places routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const HttpError = require("./models/http-error.js");
 const placesRouter = require("./routes/places-routes");
 const userRouter = require("./routes/users-routes");
 const app = express();
 
-const mongoPractice = require("./mongo/mongoose")
+// const mongoPractice = require("./mongo/mongoose")
 
 app.use(express.json());
 app.use(
@@ -13,26 +14,27 @@ app.use(
     extended: false,
   })
 );
-app.post('/products',mongoPractice.createProduct);
-app.get('/products',mongoPractice.getProducts)
-// app.use("/api/places", placesRouter);
-// app.use("/api/users", userRouter);
-// // no route found error
-// app.use((req, res, next) => {
-//   const error = new HttpError("Could not find this route", 404);
-//   return next(error);
-// });
+// app.post('/products',mongoPractice.createProduct);
+// app.get('/products',mongoPractice.getProducts)
+app.use("/api/places", placesRouter);
+app.use("/api/users", userRouter);
+// no route found error
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route", 404);
+  return next(error);
+});
 
 // // error handling middleware
 
-// app.use((error, req, res, next) => {
-//   if (res.headerSent) {
-//     return next(error);
-//   }
-//   res.status(error.code || 500);
-//   res.json({ message: error.message || "An unknown error occured!" });
-// });
-
-app.listen(5000, () => {
-  console.log("server started at port - 5000");
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured!" });
 });
+const url ="mongodb+srv://ImitaSingha:AF0opguAlOJd4EP5@cluster0.wkn3w.mongodb.net/places?retryWrites=true&w=majority";
+mongoose.connect(url,{ user: process.env.MONGO_USER, pass: process.env.MONGO_PASSWORD, useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+  app.listen(5000);
+}).catch(err=>console.log("!ERROR", err))
+
--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,75 +1,98 @@
 const HttpError = require("../models/http-error");
 const { v4: uuidv4 } = require("uuid");
 const {validationResult} = require("express-validator");
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Victoria Memorial",
-    description:
-      "Elegant, domed, white marble museum, opened in 1921, housing displays on the history of Kolkata.",
-    location: {
-      lat: 22.5448082,
-      lng: 88.0624064,
-    },
-    address:
-      "Victoria Memorial Hall, 1, Queens Way, Maidan, Kolkata, West Bengal 700071",
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Victoria Memorial 2",
-    description:
-      "2-Elegant, domed, white marble museum, opened in 1921, housing displays on the history of Kolkata.",
-    location: {
-      lat: 22.5448082,
-      lng: 88.0624064,
-    },
-    address:
-      "2-Victoria Memorial Hall, 1, Queens Way, Maidan, Kolkata, West Bengal 700071",
-    creator: "u1",
-  },
-];
+const Place = require("../models/place");
+// let DUMMY_PLACES = [
+//   {
+//     id: "p1",
+//     title: "Victoria Memorial",
+//     description:
+//       "Elegant, domed, white marble museum, opened in 1921, housing displays on the history of Kolkata.",
+//     location: {
+//       lat: 22.5448082,
+//       lng: 88.0624064,
+//     },
+//     address:
+//       "Victoria Memorial Hall, 1, Queens Way, Maidan, Kolkata, West Bengal 700071",
+//     creator: "u1",
+//   },
+//   {
+//     id: "p2",
+//     title: "Victoria Memorial 2",
+//     description:
+//       "2-Elegant, domed, white marble museum, opened in 1921, housing displays on the history of Kolkata.",
+//     location: {
+//       lat: 22.5448082,
+//       lng: 88.0624064,
+//     },
+//     address:
+//       "2-Victoria Memorial Hall, 1, Queens Way, Maidan, Kolkata, West Bengal 700071",
+//     creator: "u1",
+//   },
+// ];
 // function expression with arrow function
-const getPlaceById = (req, res, next) => {
+
+const createPlace = async (req, res, next) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    console.log("errors",errors)
+    return next(new HttpError('Invalid inputs passed, please check your data',422))
+  }
+   const { title, description, location, address, creator } = req.body;
+   const createdPlace = new Place({
+     title,
+     description,
+     image:"https://images.pexels.com/photos/1289845/pexels-photo-1289845.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+     address,
+     location: location,
+     creator
+   });
+   try{
+     await createdPlace.save();
+   }
+   catch(err ){
+    const error = new HttpError("Creating place failed, please try again.",500);
+    console.log(err);
+   return next(error);
+ }
+  
+  
+   res.status(201).json({ place: createdPlace });
+ };
+
+ const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  let place;
+  try{
+    place= await Place.findById(placeId);
+  }catch(err){
+    const error = new HttpError("Something went wrong, could not find a place", 500)
+  return next(error);
+  }
   if (!place) {
     return next(
       new HttpError("Could not find a place for the provided id !!!", 404)
     );
   }
-  res.json({ place, message: "Place details" });
+  res.json({ place: place.toObject({getters: true}) });
 };
-
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.filter((p) => p.creator === userId);
+  let places;
+  try{
+    places = await Place.find({creator:userId})
+  }catch(err){
+    const error = new HttpError("Something went wrong, could not find a place", 500)
+  return next(error);
+  }
   if (!places || places.length === 0) {
     return next(
       new HttpError("Could not find a places for the provided id !!!", 404)
     );
   }
-  res.json({ places, message: "user details" });
+  res.json({ places:places.map(place=>place.toObject({getters: true})) });
 };
-const createPlace = (req, res, next) => {
- const errors= validationResult(req);
- if(!errors.isEmpty()){
-   console.log("errors",errors)
-   return next(new HttpError('Invalid inputs passed, please check your data',422))
- }
-  const { title, description, coordinates, address, creator } = req.body;
-  const createdPlace = {
-    id: uuidv4(),
-    title,
-    description,
-    location: coordinates,
-    address,
-    creator,
-  };
-  DUMMY_PLACES.push(createdPlace);
-  res.status(201).json({ place: createdPlace });
-};
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors= validationResult(req);
  if(!errors.isEmpty()){
    console.log("errors",errors)
@@ -77,24 +100,46 @@ const updatePlace = (req, res, next) => {
  }
   const placeId = req.params.pid;
   const { title, description } = req.body;
-  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
-
-  updatedPlace.title = title;
-  updatedPlace.description = description;
-
-  DUMMY_PLACES[placeIndex] = updatedPlace;
-  res.status(200).json({ place: updatedPlace });
-};
-const deletePlace = (req, res, next) => {
-  const placeId = req.params.pid;
-  if(!DUMMY_PLACES.filter((p) => p.id !== placeId)){
-    throw new HttpError('Could not find a place for that id', 404)
+  let place;
+  try{
+    // find the place From Place model by id and store it in place variable
+    place = await Place.findById(placeId)
+  }catch(err){
+    const error = new HttpError("Something went wrong.Could not update place", 500)
+  return next(error);
   }
-  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+  // change the title, description 
+  place.title = title;
+  place.description = description;
+// now save the changed value in database
+try{
+await place.save();
+}
+  catch(err){
+    const error = new HttpError("Something went wrong.Could not update place", 500)
+  return next(error);
+  }
+  res.status(200).json({ place: place.toObject({getters:true}) });
+};
+const deletePlace = async(req, res, next) => {
+  const placeId = req.params.pid;
+ let place;
+ try{
+   place = await Place.findById(placeId);
+
+ }catch(err){
+  const error = new HttpError("Something went wrong.Could not delete place", 500)
+  return next(error);
+ }
+// remove and save the data 
+try{
+  await place.remove();
+}catch(err){
+  const error = new HttpError("Something went wrong.Could not delete place", 500)
+  return next(error);
+ } 
   res.status(200).json({ message: "Deleted successfully" });
 };
-
 exports.getPlaceById = getPlaceById;
 exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
