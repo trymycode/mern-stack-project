9030435315e7eef8d25683b69ac5bb9b947cea59
trymycode <imitasingha1994@gmail.com>
2021-07-22 21:22:09 +0530

    add auth token in delete, patch functionality from frontend and backend

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,8 +1,11 @@
 const HttpError = require("../models/http-error");
 const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
+  if(req.method === 'OPTIONS'){
+    return next();
+  }
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization.split(" ")[1];//Authorization: 'Bearer TOKEN'
     if (!token) {
       throw new Error("Authentication failed!");
     }
